d30f80556b9136e55b747137426b0aba11a11068
Daniel Write <daniel@writefamily.com>
2023-09-09 02:33:51 -0400

    removed dead code

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use clap::Parser;
 use colored::Colorize;
 
 mod lexer;
+mod errors;
 
 /// A light-weight MIPS emulator and debugger.
 #[derive(Parser, Debug)]
@@ -33,33 +34,12 @@ fn main() {
     };
 
     let program: Vec<String> = io::BufReader::new(file).lines().map(|l| l.expect("Could not parse line.")).collect();
+}
 
-    let lexed_program = lexer::read(&program);
-    if let Err(error) = lexed_program {
-        let prelude = format!("{} ({}) on line {} at {}.", "Error".red().bold(), "syntax error".bright_black(), error.line + 1, error.character + 1);
-        println!("{}", prelude);
-        let mut line = &program[error.line].to_string();
-        let mut line = line.trim().to_string();
-        let range = error.character..(error.character + error.len);
-        for idx in 0..line.len() {
-            if range.contains(&idx) {
-                print!("{}", line.chars().nth(idx).expect("This should not fail.").to_string().red());
-            } else {
-                print!("{}", line.chars().nth(idx).expect("This should not fail."));
-            }
-        }
-        println!();
-        println!("{}{} {}", " ".repeat(error.character), "^".repeat(error.len).red().bold(), error.msg);
-        return;
-    }
-
-    for node in lexed_program.unwrap() {
-        if let Node::Token(token) = node {
-            println!("{:?}", token);
-        } else if let Node::Tree(tree) = node {
-            let mut buffer = String::new();
-            tree.write_formatted(&mut buffer).unwrap();
-            println!("{}", buffer);
-        }
+pub mod code {
+    // highlight snytax for one line of code
+    pub fn syntax_highlight(code: String) -> String {
+        // note that red is only to be used for errors
+        code
     }
 }
