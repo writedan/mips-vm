97cfcdfdd943b72fecd4d30431b1fa6a66eef00f
Daniel Write <daniel@writefamily.com>
2023-09-10 13:07:01 -0400

    removed mips

--- src/parse.rs
+++ src/parse.rs
@@ -1,5 +1,4 @@
 mod ast;
-mod mips;
 
 use crate::parse::ast::*;
 use crate::lexer::tokens::CodeSegment;
@@ -14,16 +13,27 @@ type Return = ParRes<Vec<ASTNode<Token>>>;
 pub fn parse(program: &Vec<Token>) -> Return {
 	let mut nodes = Vec::new();
 
-	for token in program {
-		println!("{:#?}", token);
+	let mut idx = 0;
+	while idx < program.len() {
+		let token = &program[idx];
+		match token {
+			Token::Register(id, segment) => {
+
+
+			_ => {
+				println!("{:?}", token)
+			}
+		}
+
+		idx += 1;
 	}
 
 	Ok(nodes)
 }
 
-fn call_err(token: Token, segment: CodeSegment, msg: errors::Msg) -> Return {
+fn call_err(segment: &CodeSegment, msg: errors::Msg) -> Return {
 	Err(errors::Err {
-		segment,
+		segment: segment.clone(),
 		errtype: errors::ErrType::Assemble,
 		msg
 	})
