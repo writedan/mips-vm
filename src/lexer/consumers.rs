4ac1db95fd15ec344d54724ed70b5b6acfb7358b
Daniel Write <daniel@writefamily.com>
2023-09-09 22:04:43 -0400

    fixed error trigger

--- src/lexer/consumers.rs
+++ src/lexer/consumers.rs
@@ -20,9 +20,10 @@ impl Consumer for Directive {
 	fn consume(idx: &mut usize, lexer: &mut Lexer) -> Result<Token, errors::Err> {
 		*idx += 1; // skip the initial "."
 
-		if lexer.text.len() > 0 {
+		if lexer.buffer.len() > 0 {
 			let msgs = errors::Msg::Many(vec![
-				format!("Trying to consume directive but buffer = {}", lexer.buffer),
+				format!("Trying to consume directive but buffer length = {}", lexer.buffer.len()),
+				format!("buffer = \"{}\"", lexer.buffer.red()),
 				"Buffer should be empty.".to_string()
 			]);
 			return lexer.error(lexer.text.len(), 0, msgs);
