98ac610a203c61edb1028395089315c50bcdee7a
Daniel Write <daniel@writefamily.com>
2023-09-13 00:46:10 -0400

    changed token print

--- src/lexer/tokens.rs
+++ src/lexer/tokens.rs
@@ -27,12 +27,12 @@ pub enum Token {
 impl fmt::Display for Token {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 		match self {
-			Token::Directive(id, _) => write!(f, "Directive({})", id),
-			Token::DefLabel(id, _) => write!(f, "LabelDefinition({})", id),
-			Token::Identifier(id, _) => write!(f, "Identifier({})", id),
-			Token::Register(id, _) => write!(f, "Register({})", id),
-			Token::StringLiteral(id, _) => write!(f, "StringLiteral({:?})", id),
-			Token::NumberLiteral(num, _) => write!(f, "NumberLiteral({})", num),
+			Token::Directive(id, _) => write!(f, "Directive"),
+			Token::DefLabel(id, _) => write!(f, "DefineLabel"),
+			Token::Identifier(id, _) => write!(f, "Identifier"),
+			Token::Register(id, _) => write!(f, "Register"),
+			Token::StringLiteral(id, _) => write!(f, "StringLiteral"),
+			Token::NumberLiteral(num, _) => write!(f, "NumberLiteral"),
 			_ => panic!("Unknown token {:?}", self)
 		};
 
