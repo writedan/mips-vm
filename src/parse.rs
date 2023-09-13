acf309a45f32cdd051d978c3479c9afa75ae3c4b
Daniel Write <daniel@writefamily.com>
2023-09-13 00:46:29 -0400

    moved main logic to new function to parse one symbol

--- src/parse.rs
+++ src/parse.rs
@@ -14,25 +14,60 @@ use colored::Colorize;
 type ParRes<T> = Result<T, errors::Err>;
 type Return = ParRes<Vec<ASTNode<Symbol>>>;
 
+pub fn parse_one(idx: &mut usize, program: &Vec<Token>) -> ParRes<ASTNode<Symbol>> {
+	let token = &program[*idx];
+	let symbol: Option<ASTNode<Symbol>> = match token {
+		Token::Directive(id, segment) => {
+			match parsers::Directive::parse(idx, &program) {
+				Ok(node) => Some(node),
+				Err(err) => return Err(err)
+			}
+		},
+
+		Token::DefLabel(id, segment) => {
+			match parsers::DefLabel::parse(idx, &program) {
+				Ok(node) => Some(node),
+				Err(err) => return Err(err)
+			}
+		},
+
+		Token::Identifier(id, segment) => {
+			// we can assume this to be an instruction, because labels can only occur after instructions
+			// thus labels will be consumed elsewhere
+			None
+		}
+
+		_ => {
+			let msg = errors::Msg::One(format!("Unexpected token {}.", token.to_string().red()));
+			return Err(errors::Err {
+				segment: extract_segment(token),
+				errtype: errors::ErrType::Assemble,
+				msg
+			});
+		}
+	};
+
+	match symbol {
+		Some(symbol) => Ok(symbol),
+		None => {
+			let msg = errors::Msg::One(format!("Token failed to parse."));
+			return Err(errors::Err {
+				segment: extract_segment(token),
+				errtype: errors::ErrType::Assemble,
+				msg
+			});
+		}
+	}
+}
+
 pub fn parse(program: &Vec<Token>) -> Return {
 	let mut nodes = Vec::new();
 
 	let mut idx = 0;
 	while idx < program.len() {
-		let token = &program[idx];
-		let symbol: Option<ASTNode<Symbol>> = match token {
-			Token::Directive(id, segment) => {
-				match parsers::Directive::parse(&mut idx, &program) {
-					Ok(node) => Some(node),
-					Err(err) => return Err(err)
-				}
-			}
-			_ => None
-		};
-
-		match symbol {
-			Some(symbol) => nodes.push(symbol),
-			None => {}
+		match parse_one(&mut idx, program) {
+			Ok(symbol) => nodes.push(symbol),
+			Err(err) => return Err(err)
 		}
 
 		idx += 1;
